2e6be559805e8a986be6c0d76283f40b6cb9b71b
benjaminthorent <b.thorent@gmail.com>
2011-01-13 11:45:03 +0100

    first commit

---
+++ src/com/ei3info/graphi/courses/courses.java
@@ -0,0 +1,13 @@
+package com.ei3info.graphi.courses;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class courses extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
