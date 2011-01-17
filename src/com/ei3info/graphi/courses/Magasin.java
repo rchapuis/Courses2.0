1f6901332651d76235d96c8444ca7af43781d9ce
benjaminthorent <b.thorent@gmail.com>
2011-01-17 14:29:03 +0100

    Commit après premiers fichiers pour base de travail

---
+++ src/com/ei3info/graphi/courses/Magasin.java
@@ -0,0 +1,11 @@
+package com.ei3info.graphi.courses;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Magasin extends Activity{
+	/** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+    }
+}
