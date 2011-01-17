16587e447e94899bb3230f104d79a3b4c70717e7
benjaminthorent <b.thorent@gmail.com>
2011-01-17 15:06:41 +0100

    Dernière mise à jour avec premier écran temporaire fini

--- src/com/ei3info/graphi/courses/Listes.java
+++ src/com/ei3info/graphi/courses/Listes.java
@@ -7,7 +7,5 @@ public class Listes extends Activity {
 	/** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
     }
 }
--- src/com/ei3info/graphi/courses/courses.java
+++ src/com/ei3info/graphi/courses/courses.java
@@ -1,13 +1,71 @@
 package com.ei3info.graphi.courses;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class courses extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        
+        // Hide the window title.
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        //getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        
         setContentView(R.layout.main);
-    }
+        
+        Button acces_liste =(Button)findViewById(R.id.acces_liste);
+	    Button acces_magasin =(Button)findViewById(R.id.acces_magasin);
+	    Button accueil_aide =(Button)findViewById(R.id.accueil_aide);
+	    
+	  //On crée un écouteur d'évènement commun aux deux Button
+	    OnClickListener onClickLister = new OnClickListener() {
+	 
+	    	@Override
+	    	public void onClick(View v){
+		    	switch(v.getId()){
+		    	case R.id.acces_liste:
+		    		Intent intent = new Intent(courses.this, courses.class);
+					startActivity(intent);
+		    		finish();
+		    		break;
+		    	case R.id.acces_magasin:
+		    		Intent intent2 = new Intent(courses.this, courses.class);
+					startActivity(intent2);
+					finish();
+		    		break;
+		    	}	
+	    	};
+	    };
+	  
+    
+	    //on affecte aux Button l'écouteur d'évènement
+	    acces_liste.setOnClickListener(onClickLister);
+	    acces_magasin.setOnClickListener(onClickLister);
+	    
+	    AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage(R.string.aide_texte_courses)
+            .setCancelable(false)
+            .setTitle(R.string.aide_titre)
+            .setPositiveButton("Retour", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int id) {
+                dialog.dismiss();
+                }
+            });
+        final AlertDialog alert = builder.create();
+        
+        accueil_aide.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                alert.show();
+            }
+            });}
+
 }
